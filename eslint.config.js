f21a5ba5c2b8bcedf99f49835b6f9be7e8f5d411
Abid Ali <abidmmp99@gmail.com>
2025-06-03 09:03:07 +0000

    Improve touch handling

--- eslint.config.js
+++ eslint.config.js
@@ -40,3 +40,5 @@ export default tseslint.config(
 );
 
 const omit = (obj, keys) => Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k)));
+
+const range = (s, e, step=1) => { const r=[]; for(let i=s;i<e;i+=step) r.push(i); return r; };
