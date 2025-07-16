bd4fb66216c63cbc92271385b76a768fb617fca4
Abid Ali <abidmmp99@gmail.com>
2025-07-16 16:21:16 +0000

    Optimize DOM updates

--- eslint.config.js
+++ eslint.config.js
@@ -44,3 +44,5 @@ const omit = (obj, keys) => Object.fromEntries(Object.entries(obj).filter(([k]) 
 const range = (s, e, step=1) => { const r=[]; for(let i=s;i<e;i+=step) r.push(i); return r; };
 
 const pick = (obj, keys) => Object.fromEntries(keys.filter(k=>k in obj).map(k=>[k,obj[k]]));
+
+const debounce = (fn, delay) => { let t; return (...a) => { clearTimeout(t); t = setTimeout(() => fn(...a), delay); }; };
